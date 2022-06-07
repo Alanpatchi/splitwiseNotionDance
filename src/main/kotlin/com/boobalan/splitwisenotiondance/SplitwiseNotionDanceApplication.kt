93f6762ec881d9a6fc58fdb96521081eecfa871f
alanpatchi <boobalang1991@gmail.com>
2022-06-07 13:37:02 -0700

    First commit

---
+++ src/main/kotlin/com/boobalan/splitwisenotiondance/SplitwiseNotionDanceApplication.kt
@@ -0,0 +1,11 @@
+package com.boobalan.splitwisenotiondance
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SplitwiseNotionDanceApplication
+
+fun main(args: Array<String>) {
+    runApplication<SplitwiseNotionDanceApplication>(*args)
+}
---
+++ src/test/kotlin/com/boobalan/splitwisenotiondance/SplitwiseNotionDanceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.boobalan.splitwisenotiondance
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SplitwiseNotionDanceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
